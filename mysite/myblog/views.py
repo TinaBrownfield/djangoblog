2221dfbf72186ea8087fec13cd9e2cded11fda56
Tina B <Tina.Brownfield@cdk.com>
2019-08-06 13:19:09 -0700

    lesson7 fixes

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -20,13 +20,6 @@ def list_view(request):
     posts = published.order_by('-published_date')
     context = {'posts': posts}
     return render(request, 'list.html', context)
-# def list_view(request):
-#     published = Post.objects.exclude(published_date__exact=None)
-#     posts = published.order_by('-published_date')
-#     template = loader.get_template('list.html')
-#     context = {'posts': posts}
-#     body = template.render(context)
-#     return HttpResponse(body, content_type="text/html")
 
 def detail_view(request, post_id):
     published = Post.objects.exclude(published_date__exact=None)
