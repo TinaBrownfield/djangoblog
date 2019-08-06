c66326300db3e77684a5bbd67734a09118132fa9
Tina B <Tina.Brownfield@cdk.com>
2019-08-06 11:40:41 -0700

    lesson7 classwork database model and CRUD update

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from myblog.models import Post
+from myblog.models import Category
 
 admin.site.register(Post)
+
+admin.site.register(Category)
