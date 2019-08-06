3cba94469838ccc09b7a06ba4540921a0f5d1bd3
Tina B <Tina.Brownfield@cdk.com>
2019-08-06 12:26:26 -0700

    lesson7 listview commit

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.template import loader
+from myblog.models import Post
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+# Create your views here.
+
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
@@ -12,3 +14,11 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('list.html')
+    context = {'posts': posts}
+    body = template.render(context)
+    return HttpResponse(body, content_type="text/html")
