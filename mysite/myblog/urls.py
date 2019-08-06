45a8d3dcefd8f86cef636f561342ae75d99fbe5a
Tina B <Tina.Brownfield@cdk.com>
2019-08-06 12:35:26 -0700

    lesson7 templates and fixes

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import list_view
 from .views import stub_view
 
 urlpatterns = [
-    #path('', list_view, name="blog_index"),
-    path('', stub_view, name="blog_index"),
+    path('',list_view, name="blog_index"),
+    #path('', stub_view, name="blog_index"),
     path('posts/<int:post_id>/', stub_view, name="blog_detail"),
 ]
